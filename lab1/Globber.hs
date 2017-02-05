d51c66dbc8533dc2cb204f314a8eee656cdaf46d
何羿宏 <heyihong.cn@gmail.com>
2017-02-04 20:09:49 -0800

    finish lab1

--- lab1/Globber.hs
+++ lab1/Globber.hs
@@ -2,6 +2,68 @@ module Globber (matchGlob) where
 
 type GlobPattern = String
 
-matchGlob :: GlobPattern -> String -> Bool
-matchGlob _ _ = False
+data GlobChar = Literal Char
+            |   OneChar String
+            |   AnyChar
+            |   AnyString deriving (Show, Eq)
 
+type GlobString = [GlobChar]
+
+parseGlobString :: GlobPattern -> GlobString
+parseGlobString [] = []
+parseGlobString ('*':cs) = AnyString:parseGlobString cs
+parseGlobString ('?':cs) = AnyChar:parseGlobString cs
+parseGlobString ('[':cs) = let (s1, s2) = findParen cs in (OneChar (convertDash s1)):parseGlobString s2
+    where findParen [] = error "Invalid bracket ["
+          findParen (']':xs) = ("", xs)
+          findParen ('\\':x:xs) = let (s1, s2) = findParen xs in (x:s1, s2)
+          findParen (x:xs) = let (s1, s2) = findParen xs in (x:s1, s2)
+          convertDash [] = []
+          convertDash (l:'-':r:xs) = [l..r] ++ (convertDash xs)
+          convertDash (x:xs) = x:(convertDash xs)
+parseGlobString ('\\':c:cs) = Literal c:parseGlobString cs
+parseGlobString (c:cs) = Literal c:parseGlobString cs
+
+matchGlobChar :: GlobChar -> Char -> Bool
+matchGlobChar (Literal gc) c = gc == c
+matchGlobChar (OneChar str) c = elem c str
+matchGlobChar AnyChar _ = True
+matchGlobChar _ _ = False
+
+matchWithoutAnyString :: GlobString -> String -> Maybe String
+matchWithoutAnyString [] str = Just str
+matchWithoutAnyString (g:gs) (s:str) = if matchGlobChar g s 
+                                        then matchWithoutAnyString gs str
+                                        else Nothing
+matchWithoutAnyString _ _ = Nothing
+
+firstMatchWithoutAnyString :: GlobString -> String -> Maybe String
+firstMatchWithoutAnyString gs str = case matchWithoutAnyString gs str of
+                                        Just remain -> Just remain
+                                        Nothing -> if length str >= 1 
+                                                    then firstMatchWithoutAnyString gs (tail str)
+                                                    else Nothing
+
+splitByAnyString :: GlobString -> (GlobString, GlobString)
+splitByAnyString [] = ([], [])
+splitByAnyString (g:gs) | g == AnyString = ([], g:gs)
+                          | otherwise = let (pat, rm) = splitByAnyString gs in (g:pat, rm)
+
+matchGlobString :: GlobString -> String -> Bool
+matchGlobString [] str = length str == 0
+matchGlobString [AnyString] _ = True
+matchGlobString (AnyString:AnyString:gs) str = matchGlobString (AnyString:gs) str
+matchGlobString (AnyString:gs) str = let (pat, rm) = splitByAnyString gs
+                                        in (case firstMatchWithoutAnyString pat str of
+                                                Just remain -> matchGlobString rm remain 
+                                                Nothing -> False)
+matchGlobString gs str = let (pat, rm) = splitByAnyString gs
+                            in (case matchWithoutAnyString pat str of
+                                    Just remain -> matchGlobString rm remain 
+                                    Nothing -> False)
+
+
+
+
+matchGlob :: GlobPattern -> String -> Bool
+matchGlob gp str = matchGlobString (parseGlobString gp) str
--- lab1/TestGlobber.hs
+++ lab1/TestGlobber.hs
@@ -13,3 +13,113 @@ main = hspec $ describe "Testing Globber" $ do
       it "shouldn't match non-empty string" $
         matchGlob "" "string" `shouldBe` False
 
+    describe "literal pattern" $ do
+      it "matches string" $
+        matchGlob "string" "string" `shouldBe` True
+      it "shouldn't match string1" $
+        matchGlob "string" "stringa" `shouldBe` False
+      it "shouldn't match string2" $
+        matchGlob "string" "astring" `shouldBe` False
+      it "escaped literals 1" $
+        matchGlob "\\[a]" "[a]" `shouldBe` True
+      it "escaped literals 2" $
+        matchGlob "\\*\\*\\?" "**?" `shouldBe` True
+      it "escaped literals 3" $
+        matchGlob "\\\\a\\\\" "\\a\\" `shouldBe` True
+      it "escaped literals 4" $
+        matchGlob "ab\\*ba" "ab*ba" `shouldBe` True
+      it "escaped literals 5" $
+        matchGlob "ab\\[ba" "ab[ba" `shouldBe` True
+      it "escaped literals 6" $
+        matchGlob "ab[a\\]]ba" "ab]ba" `shouldBe` True
+      it "escaped literals 7" $
+        matchGlob "ab[a\\]]ba" "ababa" `shouldBe` True
+
+    describe "bracket pattern" $ do
+        it "simple bracket" $
+            matchGlob "a[bc]d" "acd" `shouldBe` True
+        it "contiguous bracket" $
+            matchGlob "a[b-z]d" "add" `shouldBe` True
+
+
+    -- describe "set match" $ do
+    --     describe "[ab[c]" $ do
+    --         it "matches a" $
+    --             matchGlob "[ab[c]" "a" `shouldBe` True
+    --         it "matches b" $
+    --             matchGlob "[ab[c]" "b" `shouldBe` True
+    --         it "matches [" $
+    --             matchGlob "[ab[c]" "[" `shouldBe` True
+    --         it "matches c" $
+    --             matchGlob "[ab[c]" "c" `shouldBe` True
+    --     describe "[abcd]" $ do
+    --         it "matches a" $
+    --             matchGlob "[abcd]" "a" `shouldBe` True
+    --         it "matches b" $
+    --             matchGlob "[abcd]" "b" `shouldBe` True
+    --         it "matches c" $
+    --             matchGlob "[abcd]" "c" `shouldBe` True
+    --         it "matches d" $
+    --             matchGlob "[abcd]" "d" `shouldBe` True
+    --     describe "[\\a-\\z]" $ do
+    --         it "matches a" $
+    --             matchGlob "[\\a-\\z]" "a" `shouldBe` True
+    --         it "matches b" $
+    --             matchGlob "[\\a-\\z]" "b" `shouldBe` True
+    --         it "matches y" $
+    --             matchGlob "[\\a-\\z]" "y" `shouldBe` True
+    --         it "matches z" $
+    --             matchGlob "[\\a-\\z]" "z" `shouldBe` True
+    --         it "not matches \\" $
+    --             matchGlob "[\\a-\\z]" "\\" `shouldBe` False
+    --     describe "[-abc]" $ do
+    --         it "matches a" $
+    --             matchGlob "[-abc]" "-" `shouldBe` True
+    --         it "matches b" $
+    --             matchGlob "[-abc]" "a" `shouldBe` True
+    --         it "matches c" $
+    --             matchGlob "[-abc]" "b" `shouldBe` True
+    --         it "matches z" $
+    --             matchGlob "[-abc]" "c" `shouldBe` True
+    --     describe "[abc-]" $ do
+    --         it "matches a" $
+    --             matchGlob "[abc-]" "a" `shouldBe` True
+    --         it "matches b" $
+    --             matchGlob "[abc-]" "b" `shouldBe` True
+    --         it "matches c" $
+    --             matchGlob "[abc-]" "c" `shouldBe` True
+    --         it "matches -" $
+    --             matchGlob "[abc-]" "-" `shouldBe` True
+    --     describe "[--]" $ do
+    --         it "matches -" $
+    --             matchGlob "[--]" "-" `shouldBe` True
+    --     describe "[---]" $ do
+    --         it "matches -" $
+    --             matchGlob "[---]" "-" `shouldBe` True
+    --     describe "[----]" $ do
+    --         it "matches -" $
+    --             matchGlob "[----]" "-" `shouldBe` True
+    --     describe "[a-d-z]" $ do
+    --         it "matches a" $
+    --             matchGlob "[a-d-z]" "a" `shouldBe` True
+    --         it "matches b" $
+    --             matchGlob "[a-d-z]" "b" `shouldBe` True
+    --         it "matches c" $
+    --             matchGlob "[a-d-z]" "c" `shouldBe` True
+    --         it "matches d" $
+    --             matchGlob "[a-d-z]" "d" `shouldBe` True
+    --         it "matches -" $
+    --             matchGlob "[a-d-z]" "-" `shouldBe` True
+    --         it "matches z" $
+    --             matchGlob "[a-d-z]" "z" `shouldBe` True
+    --     describe "[z-a]" $ do
+    --         it "shouldn't matches anything" $
+    --             matchGlob "[z-a]" "a" `shouldBe` False    
+    --         it "shouldn't matches anything" $
+    --             matchGlob "[z-a]" "z" `shouldBe` False  
+
+    describe "time complexity" $ do
+        it "should match" $
+            matchGlob "test1****test2****test3" ("test1" ++ take 1000 (repeat 'a') ++ "test2" ++ take 1000 (repeat 'b') ++ "test3") `shouldBe` True
+        it "shouldn't match" $
+            matchGlob "test1****test2****test" ("test1" ++ take 1000 (repeat 'a') ++ "test2" ++ take 1000 (repeat 'b') ++ "test3") `shouldBe` False 
